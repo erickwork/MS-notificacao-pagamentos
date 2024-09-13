475ad19d2081c43977968178ad92c06277cf3a63
erickwork <dery.erick@outlook.com>
2024-09-13 18:21:51 -0300

    first commit

---
+++ src/test/java/br/com/erick/notificacao/NotificacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.erick.notificacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
